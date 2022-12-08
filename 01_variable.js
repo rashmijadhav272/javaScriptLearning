448056b44f668b23a8f03b6a85cb3eabe6701125
rashmijadhav272 <rashmijadhav272@gmail.com>
2022-12-08 17:37:35 +0530

    created 01_variable-assign.js
    created 01_variable-assign.js

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var myName;//variable declaration
+myName = "mohit sharma";//variable initialization
+console.log(myName);
+
+var myFrdNmae ="sachin tendulakar";//variable declaration and initialization at the same time
+console.log(myFrdNmae);
+
+myFrdNmae ="ms dhoni";//variable update
+console.log(myFrdNmae);
+
---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+var myNmae;
+var myName ="Rashmi Jadhav";
+console.log(myName);
+
+myFullName ="Rashmi Rohit Jadhav";
+console.log(myFullName);
+
+var myLove ="I LOVE JAVASCRIPT";
+console.log(myLove);
+
+var myName ="Rashmi jadhav";
+var address ="pune";
+var Pin_code ="411062";
+var gender ="feamle";
+var is_Married ="yes";
+
+console.log(myName,address,Pin_code,gender,is_Married);
+
